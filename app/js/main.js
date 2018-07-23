bbcc73dd33e6f4f403b3fa4a1c12eb29c504e797
hohee <boycat87@gmail.com>
2018-07-23 14:14:59 +0900

    electron auto update

---
+++ app/js/main.js
@@ -0,0 +1,64 @@
+var $ = require("jquery");
+var config = require('config/config.info');
+var CronJob = require('cron').CronJob;
+
+var networkCheck = function () {
+  var paths = config.config_dev;
+  // var paths = config.config_stage;
+  // var paths = config.config_prod;
+  $.ajax({
+    type: 'get',
+    url: paths.api_host + '/check/v1/health',
+    success: function (res, state, xhr) {
+
+      if (xhr.status == 200 && location.pathname.indexOf("/networkError.html") > 0) {
+        readyFunc.stop();
+        location.href = "index.html";
+      }
+
+    },
+    error: function (error) {
+
+      if (error.readyState == 0 && location.pathname.indexOf("/networkError.html") < 0) {
+        readyFunc.stop();
+        location.href = "networkError.html";
+      }
+
+    },
+    complete: function () {
+      // readyFunc.stop();
+    }
+  });
+};
+
+var readyFunc = new CronJob({
+
+  cronTime: "0 */5 * * * *",
+  onTick: function () {
+    networkCheck();
+  },
+  start: true,
+  runOnInit: true,
+  timeZone: "Asia/Seoul"
+
+});
+
+(function () {
+
+  networkCheck();
+
+  function listener(event) {
+    window.close();
+  }
+
+  if (window.addEventListener) {
+    addEventListener("message", listener, false)
+  } else {
+    attachEvent("onmessage", listener)
+  }
+
+  $(document).on("click", "#end-btn", function () {
+    window.close();
+  });
+
+})();
